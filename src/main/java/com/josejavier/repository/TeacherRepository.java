842d9c47785931e8b3d636804d4a8010e1337e26
Jose Javier Galan <josejaviergalan2003@gmail.com>
2024-01-22 10:56:21 +0100

    starting repos

--- src/main/java/com/josejavier/repository/TeacherRepository.java
+++ src/main/java/com/josejavier/repository/TeacherRepository.java
@@ -2,7 +2,14 @@ package com.josejavier.repository;
 
 import com.josejavier.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
+
+    @Query("SELECT t FROM Teacher t WHERE t.name = ?1")
+    public Teacher findTeacherByName(String name);
+
+    @Query("SELECT t FROM Teacher t WHERE t.id = ?1")
+    Teacher findTeacherById(int id);
 
 }
