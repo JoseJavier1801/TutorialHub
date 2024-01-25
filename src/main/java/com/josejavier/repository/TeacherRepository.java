df97ca02d42ad8c9e998b64a532e9a6032382c9b
Jose Javier Galan <josejaviergalan2003@gmail.com>
2024-01-25 12:42:42 +0100

    Creacion de los sql y de la clase para encriptar contraseñas

--- src/main/java/com/josejavier/repository/TeacherRepository.java
+++ src/main/java/com/josejavier/repository/TeacherRepository.java
@@ -6,9 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
 
-    @Query("SELECT t FROM Teacher t WHERE t.username = :username")
-    Teacher findByUsername(String username);
 
-    @Query("SELECT t FROM Teacher t WHERE t.id = :id")
+    @Query("select c.username,c.name,t.biography,t.title from Client c join Teacher t on c.id=t.id where c.id = :id")
     Teacher FindbyId(int id);
+
+    @Query("UPDATE Teacher t SET t.title = :title, t.biography = :biography WHERE t.id = :id")
+    Teacher Update(Teacher t);
+
+    @Query("DELETE FROM Teacher t WHERE t.id = :id")
+    Teacher deleteTeacher(int id);
 }
