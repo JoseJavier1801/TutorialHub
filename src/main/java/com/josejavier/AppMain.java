fd6d00cdc3de19f378ea884b075a7641603f42a4
Jose Javier Galan <josejaviergalan2003@gmail.com>
2024-03-07 09:50:05 +0100

    deploy final

--- src/main/java/com/josejavier/AppMain.java
+++ src/main/java/com/josejavier/AppMain.java
@@ -5,13 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
 
 
 @SpringBootApplication
@@ -22,17 +17,13 @@ public class AppMain {
         SpringApplication.run(AppMain.class, args);
     }
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin(System.getenv("tutorialhub-neon.vercel.app")); // Origen permitido
-        configuration.addAllowedOrigin(System.getenv("tutorialhub-r6fgo2vmw-jjgs-2003s-projects.vercel.app")); // Origen permitido
-        configuration.addAllowedOrigin(System.getenv("tutorialhub-git-master-jjgs-2003s-projects.vercel.app")); // Origen permitido
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE")); // Métodos permitidos
-        configuration.setAllowedHeaders(Arrays.asList("*")); // Todos los encabezados permitidos
-        configuration.setAllowCredentials(false); // Permitir credenciales
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/", configuration);
-        return source;
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("https://tutorialhub-neon.vercel.app").allowedMethods("*").allowedHeaders("*");
+            }
+        };
     }
 
 }
