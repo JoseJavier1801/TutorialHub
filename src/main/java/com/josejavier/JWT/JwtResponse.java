474ac0e2f2412b2c24a854855accac95aecfbaf4
Jose Javier <josejaviergalan2003@gmail.com>
2024-04-22 10:51:31 +0200

    JWT FUNCIONAL

---
+++ src/main/java/com/josejavier/JWT/JwtResponse.java
@@ -0,0 +1,15 @@
+package com.josejavier.JWT;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class JwtResponse {
+
+    private String token;
+
+}
